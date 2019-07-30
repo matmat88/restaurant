27f788b1bffa2071707ce19b366f6b394eb01a3e
Votre Nom <Vous@exemple.com>
2019-07-30 17:03:14 +0200

    actu

--- application/models/IngredientModel.class.php
+++ application/models/IngredientModel.class.php
@@ -1,46 +1,6 @@
 <?php
 class Ingredient {
-    const QUERY_ADDINGREDIENT = 'SELECT *
-    FROM ingredient
-    WHERE 1';
-    // const QUERY_DISPLAYSINGLECOMMANDE=' SELECT *
-    // FROM utilisateurs AS u
-    // INNER JOIN commande AS c
-    // ON b.auteur = u.id
-    // LEFT JOIN commentaires AS c
-    // ON b.id = c.lien
-    // WHERE b.id=?';
-    const QUERY_DISPATCHER = ' SELECT *
-    FROM commande AS c
-    inner join utilisateur AS u
-    on c.client_id = u.id;
-    WHERE u.role=livreur
-    AND u.statut=true';
 
-    protected $id;
-    protected $client_id;
-    protected $prix;
-    protected $dateDeCommande;
-    protected $statut;
-    public function findAll(PDO $dataBase){
-        $query= $dataBase->prepare(QUERY_DISPLAYCOMMANDE);
-        $error = $query->execute();
-        $result = $query -> fetchAll();
-        return $result;
-    }
-    // public function find(PDO $dataBase, int $id){
-    //     $query= $dataBase->prepare(QUERY_DISPLAYSINGLECOMMANDE);
-    //     $error = $query->execute([$id]);
-    //     $result = $query -> fetchAll();
-    //     return $result;
-    // }
-    public function findAll(PDO $dataBase){
-        $query= $dataBase->prepare(QUERY_DISPATCHER);
-        $error = $query->execute();
-        $result = $query -> fetchAll();
-        return $result;
-    }
-} 
 
 }
 ?>
