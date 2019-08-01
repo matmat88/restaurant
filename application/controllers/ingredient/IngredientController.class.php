bf1aafae0583ba8855b764f926d5e18713c00e9d
Votre Nom <Vous@exemple.com>
2019-08-01 15:17:06 +0200

    modif pour l'ajout d'ingredient + debut code pour modifier la formule

--- application/controllers/ingredient/IngredientController.class.php
+++ application/controllers/ingredient/IngredientController.class.php
@@ -9,8 +9,21 @@ class IngredientController
 
     public function httpPostMethod(Http $http, array $formFields)
     {
-    	$model = new IngredientModel(new Database());
+        $model = new IngredientModel(new Database());
+        $modelMedia = new MediaModel(new Database());
+        
+        var_dump($_FILES); die;
 
-		$ingredient = $model->addIngredient($formFields['name'], $formFields['prix'], $formFields['type']);
+        if ($_FILES["picture"]["error"] == UPLOAD_ERR_OK) {
+            $tmp_name = $_FILES["picture"]["tmp_name"];
+            // basename() peut empêcher les attaques "filesystem traversal";
+            // une autre validation/néttoyage du nom de fichier peux être appropriée
+            $name = basename($_FILES["picture"]["name"]);
+            move_uploaded_file($tmp_name, "www/images/illustrations/$name");
+        }
+        
+        $idMedia = $modelMedia->addMedia($name, $_FILES["picture"]["type"]);
+
+		$ingredient = $model->addIngredient($formFields['name'], $formFields['prix'], $formFields['type'], $idMedia);
     }
 }
