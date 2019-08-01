f104dc5e1beadedaeebbb414ce0655078fe0009b
Your Name <you@example.com>
2019-08-01 17:02:54 +0200

    17h02 01/08

--- application/controllers/dessert/DessertController.class.php
+++ application/controllers/dessert/DessertController.class.php
@@ -10,8 +10,22 @@ class DessertController
 
 public function httpPostMethod(Http $http, array $formFields)
     {
-		$model = new IngredientModel (new Database());
+        $model = new IngredientModel (new Database());
+        $modelMedia = new MediaModel(new Database());
+
+
+        if ($_FILES["picture"]["error"] == UPLOAD_ERR_OK) {
+            $tmp_name = $_FILES["picture"]["tmp_name"];
+            // basename() peut empêcher les attaques "filesystem traversal";
+            // une autre validation/néttoyage du nom de fichier peux être appropriée
+            $name = basename($_FILES["picture"]["name"]);
+            move_uploaded_file($tmp_name, ROOT_PATH."/application/www/images/illustrations/$name");
+        }
+        
+        $idMedia = $modelMedia->addMedia($name, $_FILES["picture"]["type"]);
         $user = $model -> addDessert ($formFields['name'], $formFields['prix'], $formFields['type']);
+
+
 
 		$http->RedirectTo('/ingredient');
         
